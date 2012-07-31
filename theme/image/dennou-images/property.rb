588e2338f0ad042a71734fc6e4083575cc0b7034
Youhei SASAKI <uwabami@gfd-dennou.org>
2012-07-31 23:50:00 +0900

    Create gh-pages
    Signed-off-by: Youhei SASAKI <uwabami@gfd-dennou.org>

--- theme/image/dennou-images/property.rb
+++
@@ -1,4 +0,0 @@
-@category = N_("Image")
-@title = N_("DennouImage")
-@abstract = N_("Images related to GFD Dennou Club")
-@description = N_("Provides images related to GFD Dennou Club.")
--- theme/theme/dennou/property.rb
+++
@@ -1,3 +0,0 @@
-@category = N_("Theme")
-@title = N_("GFD Dennou Club")
-@description = N_("GFD Dennou Club theme")
